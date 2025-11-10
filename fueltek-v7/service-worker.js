11874cf1dbb08ecd92c0b4af3c789031590c8e97
CryptoWorld <cryptoworldx9@gmail.com>
2025-11-09 22:09:29 -0300

    Update service-worker.js

--- fueltek-v7/service-worker.js
+++ fueltek-v7/service-worker.js
@@ -1,1 +1,27 @@
+const CACHE = "fueltek-static-v1";
+const FILES = [
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/js/main.js",
+  "/js/db.js",
+  "/js/ui.js",
+  "/js/backup.js",
+  "/assets/logo-fueltek.png",
+  "/manifest.json"
+];
 
+self.addEventListener("install", evt=>{
+  evt.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", evt=>{
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+  );
+});
+
+self.addEventListener("activate", evt=>{
+  evt.waitUntil(clients.claim());
+});
